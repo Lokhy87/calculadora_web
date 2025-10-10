ba1044042614216fb69326e4279ef4d61e738029
Lokhy87 <a.bolta@gmail.com>
2025-10-10 19:51:45 +0200

    Añadida funciones suma y resta

---
+++ calculadora.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $num1 = (float)$_POST["num1"];
+  $num2 = (float)$_POST["num2"];
+  $operacion = $_POST["operacion"];
+  $resultado = "";
+
+  if ($operacion == "suma") {
+    $resultado = $num1 + $num2;
+  } elseif ($operacion == "resta") {
+    $resultado = $num1 - $num2;
+  }
+}
+?>
