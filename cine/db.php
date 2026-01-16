a17d6400e038e986068713c08d16ec0fddba0ae4
Lokhy87 <a.bolta@gmail.com>
2026-01-16 08:07:20 +0100

    cambios

---
+++ cine/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "cine";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
